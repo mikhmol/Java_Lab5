49452836a5c31cd80c116c4afd212eb648635a99
mikhmol <mikhailo.moliakov@gmail.com>
2024-01-04 03:47:16 +0200

    Add new classes

--- WordCount.java
+++ WordCount.java
@@ -1,39 +1,20 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class WordCount {
-
-    public static void main(String[] args) {
+        public static void processText(List<Sentence> sentences, String[] words) throws IllegalArgumentException {
         try {
-            StringBuffer inputText = new StringBuffer("Boys next door. Three hundred bucks is three hundred bucks. Boy Boy. Boyfriend come here. Boy. Boyfriend. Three bucks.");
-            String[] words = {"Boy", "Three", "bucks"};
-
-            processText(inputText, words);
-        } catch (Exception e) {
-            System.out.println("Виникла помилка: " + e.getMessage());
-        }
-    }
-
-    public static void processText(StringBuffer inputText, String[] words) throws IllegalArgumentException {
-        try {
-            if (inputText == null || words == null || words.length == 0) {
+            if (sentences == null || words == null || words.length == 0) {
                 throw new IllegalArgumentException("Невірні вхідні дані");
             }
 
             Map<String, Integer> wordCountMap = new HashMap<>();
 
-            // divide String Buffer into sentences
-            int start = 0;
-            for (int i = 0; i < inputText.length(); i++) {
-                if (inputText.charAt(i) == '.') {
-                    String sentence = inputText.substring(start, i).trim();
-                    start = i + 1;
-
-                    for (String word : words) {
-                        if (sentence.matches(".*\\b" + word + "\\b.*")) {
-                            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
-                        }
-                    }
+            for (Sentence sentence : sentences) {
+                for (String word : words) {
+                    int wordCountInSentence = countWordInSentence(sentence, word);
+                    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + wordCountInSentence);
                 }
             }
 
@@ -45,5 +26,14 @@ public class WordCount {
         } catch (Exception e) {
             throw new IllegalArgumentException("Помилка обробки тексту: " + e.getMessage());
         }
+    }
+    private static int countWordInSentence(Sentence sentence, String word) {
+        int count = 0;
+        for (Word sentenceWord : sentence.getWordsList()) {
+            if (sentenceWord.getWordAsString().equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
